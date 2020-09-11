c0ab0026cafe9e171369a776a5ee3dfe8f19b3f4
Josh Morgan <jpm3338@gmail.com>
2020-09-10 20:56:45 -0400

    moved files to correct folder

--- apply_imnormalize.m
+++
@@ -1,8 +0,0 @@
-function outarray = apply_imnormalize(inarray)
-%inarray is an NxMx3 uint8 image and outarray is NxMx3;
-%Normalize all pixel values in inarray to be within range [-0.5,0.5]
-
-outarray = (double(inarray) ./ 255) - 0.5;
-
-end
-
