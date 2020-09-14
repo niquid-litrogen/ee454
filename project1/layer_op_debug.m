189b284a6f15973c6c70b653197d510e262199c2
Josh Morgan <jpm3338@gmail.com>
2020-09-13 21:39:28 -0400

    added second test for end-to-end-pass

--- project1/layer_op_debug.m
+++ project1/layer_op_debug.m
@@ -1,4 +1,7 @@
 %This script tests layer operations on the provided debugging data.
+
+%TEST 1:
+%Check that each layer outputs the correct result, given the correct input.
 %Layers with incorrect outputs are displayed in the command window. If all
 %layers provide correct outputs, no messages are displayed. 
 
@@ -7,6 +10,9 @@ load 'debuggingTest.mat';
 
 num_layers = length(layertypes);
 
+%Perform each layer operation in sequence, using the correct input at each
+%stage. If a layer produces an incorrect result, a message is displayed. If no
+%layers produce incorrect results, no messages are displayed.
 for i = 1:1:num_layers
     layertype = layertypes{i};
     layerout = layerResults{i};
@@ -59,4 +65,54 @@ for i = 1:1:num_layers
         end        
         
     end
+end
+
+%TEST 2:
+%Check that, when doing a full forward pass on debug image, the layer
+%operations produce the correct final result. A message is displayed 
+%indicating whether or not the test is passed.
+
+%used to store output of previous layer, which is used as the input to the
+%current layer
+previous_out = [];
+
+%perform each layer operation in sequence, with the sequence provided by
+%the 'layertypes' cell array.
+for i = 1:1:num_layers
+    layertype = layertypes{i};
+    layerfilter = filterbanks{i};
+    layerbias = biasvectors{i};
+    
+    if (strcmp(layertype,'imnormalize'))
+        layer_out = apply_imnormalize(imrgb);
+   
+    elseif (strcmp(layertype,'relu'))
+        layer_out = apply_relu(previous_out);
+    
+    elseif (strcmp(layertype,'maxpool'))
+        layer_out = apply_maxpool(previous_out);
+    
+    elseif (strcmp(layertype,'convolve'))
+        layer_out = apply_convolve(previous_out,layerfilter,layerbias);
+    
+    elseif (strcmp(layertype,'fullconnect'))
+        layer_out = apply_fullconnect(previous_out,layerfilter,layerbias);
+
+    elseif (strcmp(layertype,'softmax'))
+        layer_out = apply_softmax(previous_out);
+        
+    end
+    
+    previous_out = layer_out;
+    
+end    
+
+final_out = previous_out;
+
+%check that output of full end-to-end pass is equal to the provided final result,
+%within tolerance of 10 * eps
+if all(abs(final_out - layerResults{num_layers}) < 10 * eps, 'all')
+    disp('correct final output of full pass')
+else
+    disp('incorrect final output of full pass')
 end
