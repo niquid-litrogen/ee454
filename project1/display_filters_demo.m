182e5eccd9d04d06cd41ff534eae3439c0ae0086
Josh Morgan <jpm3338@gmail.com>
2020-09-22 21:35:52 -0400

    added script that displays layer 1 filters

---
+++ project1/display_filters_demo.m
@@ -0,0 +1,22 @@
+%Display RGB filters for first convolution layer of neural network. Filters in deeper layers layers of the 
+%classfier are not displayed, because they do not process the image directly and are therefore harder to interpret.
+
+%load pretrained model parameters
+load 'data_files/CNNparameters.mat'
+
+%Extract first filters from first conv layer
+filts = filterbanks{2};
+
+%Display each of 10 filters in a separate cell of a 2x5 image.
+for i = 1:10
+    filt_rgb = filts(:,:,:,i);
+    subplot(2,5,i);
+    
+    %Display rgb image, and set it to fit the entire subplot.
+    %Reference: https://www.mathworks.com/help/images/ref/truesize.html
+    imshow(filt_rgb,'Initialmagnification','fit');
+    title(sprintf('filter %d',i));
+
+end
+
+sgtitle("RGB Filters in First Convolution Layer of CNN");
