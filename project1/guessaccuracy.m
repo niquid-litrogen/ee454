c01d00c8df15d9f2e16c22c1be371c54c5c4222c
unknown <lukeghvac@gmail.com>
2020-09-27 20:55:29 -0400

    Slight code modificatons/add comments

--- project1/guessaccuracy.m
+++ project1/guessaccuracy.m
@@ -1,13 +1,18 @@
 function accurate = guessaccuracy(guess_matrix)
-    success = 0;
-    total = 0; 
-    for i=1:size(guess_matrix, 1)
-        for j=1:size(guess_matrix, 2)
-            if(j == i)
-                success = success + guess_matrix(i, j);
-            end
-            total = total + guess_matrix(i, j);
+%A function to evaluate the accuracy of a confusion matrix for a given
+%"guess".
+%Input: guess_matrix is 10x10 double
+%
+%Output: accurate is a 1x1 (scalar) double
+success = 0;
+total = 0; 
+for i=1:size(guess_matrix, 1)
+    for j=1:size(guess_matrix, 2)
+        if(j == i)
+            success = success + guess_matrix(i, j);
         end
+        total = total + guess_matrix(i, j);
     end
-    accurate = success/total;
+end
+accurate = success/total;
 end
--- project1/guessmatrix.m
+++ project1/guessmatrix.m
@@ -1,10 +1,19 @@
 function outmatrix = guessmatrix(guessnum, probability, reality)
-    outmatrix = zeros(10, 10);
-    for i=1:length(reality)
-        [B, I] = sort(probability(i,:), 'descend');
-        row = reality(i);
-        col = I(guessnum);
-        outmatrix(row, col) = outmatrix(row, col) + 1;
-    end
+% A function that creates a confusion matrix for a given probability
+% ranking. So if looking for the probability that the CNN correctly
+% classifies the object with the 3rd highest probability, the "guessnum"
+% would be 3. 
+%
+% Input: guessnum is integer from 1-10; probability is 10x10000 double; reality is 1x10000 double 
+%
+% Output: Outmatrix is a 10x10 double with CNN classifications corresponding to columns and
+% true image classes corresponding to rows.
+outmatrix = zeros(10, 10);
+for i=1:length(reality)
+    [~, I] = sort(probability(i,:), 'descend');
+    row = reality(i);
+    col = I(guessnum);
+    outmatrix(row, col) = outmatrix(row, col) + 1;
+end
 
 end
