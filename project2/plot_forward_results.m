c6cb20c64010e034f79042e1458aa176a9d05351
Josh Morgan <jpm3338@gmail.com>
2020-10-13 11:48:41 -0400

    simplified demo to only include skeleton plots

--- project2/plot_forward_results.m
+++ project2/plot_forward_results.m
@@ -1,13 +1,10 @@
 %Demo to display projected points on an image plane, for two camera models.
-%In a future revision, the skeleton demo should completely replace the
-%points only demo.
 
 %Specify an input frame number
 %Note that, in project description, we are allowed to ignore frames that do
-%not have values of all 1 in the confidence score. In a revision, think
-%about selecting specific frames that meet this requirement. In a full
-%demo, simply discard frames that do not have all joints.
-mocapFnum = 2500; %Note that the skeleton plotting will not work if you use a frame without all 12 joints
+%not have values of all 1 in the confidence score. When inputting your own frame number, this demo may not work 
+%if you use a frame that does not contain data for all 12 joints. In a full demo, simply discard frames that do not have all joints.
+mocapFnum = 15000; %It is assumed that this frame contains data for all 12 joints2
 
 %Load data
 
@@ -28,7 +25,7 @@ load('data_files\Vue4CalibInfo.mat'); %contains 'vue4' structure
 points_3D = squeeze(mocapJoints(mocapFnum,:,:));
 %Index identifying which points to pass to projection function. These
 %points have a confidence score of '1'.
-in_frame = points_3D(:,4) == 1;
+%in_frame = points_3D(:,4) == 1;
 
 subplot(1,2,1);
 
@@ -36,20 +33,20 @@ subplot(1,2,1);
 
 %(50/100) factor handles frame rate difference between 3D point data and mp4 data
 vue2video.CurrentTime = (mocapFnum-1)*(50/100)/vue2video.FrameRate;
-vid2Frame = readFrame(vue2video);
+vue2vid2Frame = readFrame(vue2video);
 
-image(vid2Frame);
+image(vue2vid2Frame);
 hold on
 
-%Project and plot 3D points on top of vue2 RGB image
+%Project and plot points on top of vue2 RGB image
 
 %Shape is (num points, 3). The columns of points_2D contain homogeneous
 %coordinates.
-points_2D_vue2 = forward_project(points_3D(in_frame,:),vue2);
+points_2D_vue2 = forward_project(points_3D,vue2);
 
-%In a future revision, I will work on making a skeleton out of the plotted
-%points.
-scatter(points_2D_vue2(1,:),points_2D_vue2(2,:),'filled');
+%pass only (x,y) coordinates instead of homogeneous coordinates
+[skel_x1,skel_y1] = make_skeleton_2D(points_2D_vue2(1:2,:));
+plot(skel_x1,skel_y1,'-ro');
 title('vue2 forward projection');
 
 subplot(1,2,2);
@@ -63,42 +60,13 @@ vue4vid2Frame = readFrame(vue4video);
 image(vue4vid2Frame);
 hold on
 
-%Project and plot 3D points on top of vue4 RGB image
+%Project and plot points on top of vue4 RGB image
 
 %Shape is (num points, 3). The columns of points_2D contain homogeneous
 %coordinates.
-points_2D_vue4 = forward_project(points_3D(in_frame,:),vue4);
+points_2D_vue4 = forward_project(points_3D,vue4);
 
-%In a future revision, I will work on making a skeleton out of the plotted
-%points.
-scatter(points_2D_vue4(1,:),points_2D_vue4(2,:),'filled');
-title('vue4 forward projection');
-
-%Here is a figure that connects the joints into a skeleton. In a future
-%revision, this should completely replace the points only demo.
-
-figure(2);
-
-%display vue2 skeleton
-
-subplot(1,2,1);
-
-image(vid2Frame);
-hold on
-
-[skel_x1,skel_y1] = make_skeleton_2D(points_2D_vue2(1:2,:));
-plot(skel_x1,skel_y1,'-ro');
-title('vue2 forward projection');
-
-%display vue4 skeleton
-
-subplot(1,2,2);
-
-image(vue4vid2Frame);
-hold on
-
-%don't pass homogeneous coordinates
+%pass only (x,y) coordinates instead of homogeneous coordinates
 [skel_x2,skel_y2] = make_skeleton_2D(points_2D_vue4(1:2,:));
 plot(skel_x2,skel_y2,'-ro');
 title('vue4 forward projection');
-
