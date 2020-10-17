281959a43a8b99a801b804b6347a4b41d6ccd74e
Luke <lukeghvac@gmail.com>
2020-10-17 00:45:48 -0400

    Updated to add F matrix

--- project2/get_epipoles.m
+++ project2/get_epipoles.m
@@ -1,16 +1,23 @@
-function [E] = get_epipoles(cam1, cam2)
+function [E, F] = get_epipoles(left, right)
 %Find and plot epipolar lines for vue2 and vue4
 
 load('data_files\Vue2CalibInfo.mat'); %contains 'vue2' structure
 load('data_files\Vue4CalibInfo.mat'); %contains 'vue4' structure
 
 %for cam2 translated to -> cam1, cam2 matrix inverted/transposed:
-R = cam1.Rmat*transpose(cam2.Rmat);
+R = left.Rmat*transpose(right.Rmat);
 %location of cam2
-loc = cam1.Pmat*[cam2.position(1); cam2.position(2); cam2.position(3); 1];
+loc = left.Pmat*[right.position(1); right.position(2); right.position(3); 1];
 Tx = loc(1);
 Ty = loc(2);
 Tz = loc(3);
 S = [0 -Tz Ty; Tz 0 -Tx; -Ty Tx 0];
 E = R*S;
+M_r = right.Kmat;
+M_r(1,1) = M_r(1,1) / right.foclen;
+M_r(2,2) = M_r(2,2) / right.foclen;
+M_l = left.Kmat;
+M_l(1,1) = M_l(1,1) / left.foclen;
+M_l(2,2) = M_l(2,2) / left.foclen;
+F = transpose(inv(M_r))*E*inv(M_l);
 
