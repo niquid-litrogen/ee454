11c2c7611e3af314a5a001dd15c443eedae49160
Ben Cheng <bcheng985@gmail.com>
2020-10-26 21:56:00 -0400

    Adjust metrics output to be more clear about combined metrics

--- project2/main_routine.m
+++ project2/main_routine.m
@@ -8,6 +8,9 @@ metrics_titles = ["Mean" "Std Dev" "Min" "Median" "Max"];
 
 filename = 'metrics_table.xlsx';
 writematrix(metrics_titles, filename, 'Range', 'A1');
-writematrix(metrics, filename, 'Range', 'A2');
+writematrix(metrics(1:end-1,:), filename, 'Range', 'A2');
+
+writematrix('Combined ' + metrics_titles, filename, 'Range', 'A16')
+writematrix(metrics(end,:), filename, 'Range', 'A17');
 
 end
