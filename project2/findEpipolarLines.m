5d6770e6ee24c0e3244be0dde1755b9cdc045f6a
Luke Garrett <lukeghvac@gmail.com>
2020-10-28 23:30:04 -0400

    Fixed implementation of findEpipolarLines.m and created demo that utilizes it, called demo_epipolar_lines_2.m

--- project2/findEpipolarLines.m
+++ project2/findEpipolarLines.m
@@ -1,17 +1,21 @@
 function [EpipolarLines1, EpipolarLines2] = findEpipolarLines(worldCoord3DPoints, cam1, cam1PixelCoords, cam2, cam2PixelCoords)
-% Given two points on an epipolar line, returns x and y coordinates that can
-% be plotted.
-%
+% Given two cameras, along with sets of forward projected points, outputs 
+% vectors [a b c]' that corresponsd to epipolar lines of the form:
+% ax + by + c = 0
 % Inputs:
-%    point_2D: 3x1 array, homogenous coordinates of a joint projected into a camera view.
-%    epipole: 3x1 array, epipole location in homogenous camera coordinates.
+%    worldCoord3DPoints: Set of M input 3D points (Mx4)
+%    cam1: Struct containing parameters for camera 1.
+%    cam1PixelCoords: Forward projected pixel coords for camera 1.
+%    cam2: Struct containing parameters for camera w.
+%    cam2PixelCoords: Forward projected pixel coords for camera 2.
 % Outputs:
-%    epipolar_line_x: array of x coordinates along epipolar line with input endpoints. 
-%    epiolar_line_y: array of y coordinates along epipolar line with input
-%                     endpoints.
+%    EpipolarLines1: 3xM vector of line equations for camera 1, where M is
+%    number of input points.
+%    EpipolarLines2: 3xM vector of line equations for camera 2, where M is
+%    the number of input points.
 
-%vector of shape (1,num_lines) containing slopes of epipolar lines)
-[~, num_points] = size(worldCoord3dPoints)
+%vector of shape (3,num_points) containing number of input points
+[~, num_points] = size(worldCoord3DPoints');
 
 
 %Homogenous world coordinates, which is necessary for
@@ -28,11 +32,11 @@ cam2_1_epipole = forward_project(cam2_homog,cam1);
 
 for i = 1:1:num_points
 %Slope of epipolar line in cam1 image plane
-slope_1 = (cam2_1_epipole(2) - cam1PixelCoords(:,2))/(cam2_1_epipole(1) - cam1PixelCoords(:,1));
+slope_1 = (cam2_1_epipole(2) - cam1PixelCoords(2,i))/(cam2_1_epipole(1) - cam1PixelCoords(1,i));
 %Slope of epipolar line in cam2 image plane
-slope_2 = (cam1_2_epipole(2) - cam2PixelCoords(:,2))/(cam1_2_epipole(1) - cam2PixelCoords(:,1));
+slope_2 = (cam1_2_epipole(2) - cam2PixelCoords(2,i))/(cam1_2_epipole(1) - cam2PixelCoords(1,i));
 
-EpipolarLines1(:, i) = [slope_1; -1; (cam1PixelCoords(2) - slope_1*cam1PixelCoords(1))];
-EpipolarLines2(:, i) = [slope_2; -1; (cam2PixelCoords(2) - slope_2*cam2PixelCoords(1))];
+EpipolarLines1(:, i) = [slope_1; -1; cam2_1_epipole(2)-(slope_1*cam2_1_epipole(1))];
+EpipolarLines2(:, i) = [slope_2; -1; cam1_2_epipole(2)-(slope_2*cam1_2_epipole(1))];
 end
 
