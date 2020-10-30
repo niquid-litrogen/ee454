6f5ae5f13f9614ac37040bf975c8fb5de85a5f4a
Josh Morgan <jpm3338@gmail.com>
2020-10-29 22:07:02 -0400

    updated projection functions

--- project2/project3DTo2D.m
+++ project2/project3DTo2D.m
@@ -3,22 +3,26 @@ function projected2DPoints = project3DTo2D(cam, worldCoord3DPoints)
 % camera parameters.
 %
 % Inputs:
-%    points_3D: 2D array of shape (num samples, 4), where each row represents a 3D point in world
-%               coordinates. The last column contains only 1s, meaning that
-%               only points with a high confidence are input.               
-%    cam_vue: Structure containing intrinsic and extrinsic camera
-%             parameters.
+%    worldCoord3DPoints: 2D array of shape (3, num samples), where each column represents a 3D point in world
+%                        coordinates. The last column contains only 1s, meaning that
+%                        only points with a high confidence are input.               
+%    cam: Structure containing intrinsic and extrinsic camera
+%         parameters.
 % Outputs:
-%    points_2D: 2d array of shape (3, num samples). Each column contains
-%    homogeneous coordinates for a projected point, in image plane of input
-%    camera model.
+%    projected2DPoints: 2d array of shape (3, num samples). Each column contains
+%                       homogeneous coordinates for a projected point, in image plane of input
+%                       camera model.
 
-%Use transpose of points_3D so that shapes line up
-projected2DPoints = cam.Kmat*cam.Pmat*worldCoord3DPoints';
+%number of points is the number of columns in the input points array
+num_points = size(worldCoord3DPoints,2);
+
+%Add a row of ones to the end of WorldCoord3DPoints before performing this
+%calculation. This represents the input points as homogeneous coordinates,
+%and is neccesary for the matrix shapes to line up.
+projected2DPoints = cam.Kmat*cam.Pmat*[worldCoord3DPoints;ones(1,num_points)];
 %Divide by Z coordinates to get true perspective projection. This command
 %performs an elementwise division between each row of points_2D and the last
 %row of points_2D (the Z coordinates).
 projected2DPoints = projected2DPoints ./ projected2DPoints(3,:);
 
 end
-
--- project2/reconstruct3DFrom2D.m
+++ project2/reconstruct3DFrom2D.m
@@ -4,24 +4,20 @@ function recovered3DPoints = reconstruct3DFrom2D(cam1, cam1PixelCoords,  cam2, c
 %
 % Inputs:
 %    cam1,cam2: structure containing camera parameters
-%    cam1_points_2D,cam2_points_2D: 2D arrays containing homogenous 2D projection coordinates of each point. These each have shape (3, num samples).
-%                                   cam1_points_2D contains points in the film plane of cam1, and cam2_points_2D contains points in the film plane of cam2.
+%    cam1_PixelCoords, cam2_PixelCoords: 2D arrays containing homogenous 2D projection coordinates of each point. These each have shape (3, num samples).
+%                                        cam1_points_2D contains points in the film plane of cam1, and cam2_points_2D contains points in the film plane of cam2.
 % Outputs:
-%    points_3D: 2D array of shape (num samples, 4), containing homogenous
-%               coordinates for 3D points in world coordinates. Coordinates are given
-%               across rows, instead of columns, for consistency of shape with input 3D data. 
+%    recovered3DPoints: 2D array of shape (3, num samples), containing
+%                       coordinates for 3D points in world coordinates. Coordinates are given
+%                       across columns. 
 
-num_points = size(cam1PixelCoords,2);
-
+%Get viewing rays, parameterized by camera location and ray directions.
+%View directions are 3D unit vectors.
 [cam1_loc,cam1_view_directions] = get_viewing_rays(cam1,cam1PixelCoords);
 [cam2_loc,cam2_view_directions] = get_viewing_rays(cam2,cam2PixelCoords);
 
-intersections = get_pseudo_intersections(cam1_loc,cam1_view_directions,cam2_loc,cam2_view_directions);
-
-%take transpose of intersections to make returned shape consistent with
-%groundtruth 3D points. Add a column of ones to return homogeneous
-%coordinates. 
-recovered3DPoints = [intersections', ones(num_points,1)];
+%Triangulate the two viewing rays. This has shape (3, num samples). 
+recovered3DPoints = get_pseudo_intersections(cam1_loc,cam1_view_directions,cam2_loc,cam2_view_directions);
 
 end
 
