3bdd2f19b8209dc40ed3411dac972f4c318b0ec1
Josh Morgan <jpm3338@gmail.com>
2020-11-17 22:16:18 -0500

    fixed comment in threshold

--- project3/threshold.m
+++ project3/threshold.m
@@ -1,9 +1,11 @@
  function out_arr = threshold(in_arr, gamma)
-%Convert input array to binary based off of threshold.
+%Convert input array to binary based pixel threshold parameter.
 %inputs:
 %    in_arr: MxN input array. It is assumed that all values are positive.
 %    gamma: Positive scalar threshold. All values in "in_arr" < "gamma" are set to 0. All
 %           values in "in_arr" >= gamma are set to 1.
+%outputs:
+%    out_arr: MxN binary array
 
 out_arr = in_arr;
 out_arr(out_arr < gamma) = 0;
