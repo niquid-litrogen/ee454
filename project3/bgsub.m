a7e327983945823f0e93fed935f265c389b41199
amk6451 <44247590+amk6451@users.noreply.github.com>
2020-12-07 21:55:57 -0500

    Update bgsub.m
    removed extra spacing in comments to fit on a single line.

--- project3/bgsub.m
+++ project3/bgsub.m
@@ -1,6 +1,5 @@
 function outimg = bgsub(B0, frame, abs_diff_threshold)
-%Perform one iteration of background subtraction motion detection
-%algorithm.
+%Perform one iteration of background subtraction motion detection algorithm.
 %Inputs:
 %    B0: double MxN reference background frame. This must be grayscale, with
 %        values ranging from 0 to 255.
@@ -9,8 +8,7 @@ function outimg = bgsub(B0, frame, abs_diff_threshold)
 %    abs_diff_threshold: Positive scalar, between 0 and 255, specifying the difference 
 %                        threshold for motion detection. 
 %Outputs:
-%    outimg: MxN binary image. A pixel with a value of '1' indicates
-%            detected motion.
+%    outimg: MxN binary image. A pixel with a value of '1' indicates detected motion.
 
     diff = abs(B0 - frame);
     %Convert 'diff' to binary image. Pixels with values > abs_diff_threshold are set to 1, indicating motion. 
